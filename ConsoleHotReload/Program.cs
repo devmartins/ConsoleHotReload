162a3c2291b3a044a3cda41f4c075830996020f4
Ildyone <ildyone@gmail.com>
2022-06-04 16:34:34 -0300

    Commit inicial

---
+++ ConsoleHotReload/Program.cs
@@ -0,0 +1,16 @@
+﻿
+void ExibirMensagem() =>
+    Console.WriteLine("Testando Hot Reload. Incrível");
+
+ConsoleKeyInfo tecla;
+
+Console.WriteLine("Aperte uma tecla\r\n");
+
+while (true)
+{
+    tecla = Console.ReadKey();
+
+    if (tecla.Key == ConsoleKey.Escape) break;
+
+    ExibirMensagem();
+}
